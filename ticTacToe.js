0ddfe3005ba685c690196e86ecb6f2cf0f82afc8
jmc030911 <jmc030911@gmail.com>
2023-01-31 18:51:49 -0800

    Add Tic-Tac-Toe

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,11 +1,10 @@
-
+const cells = document.querySelectorAll('.cell');
+const cell1 = document.querySelector('.cell1');
+const cell2 = document.querySelector('.cell2');
 
 const gameboardObj = (() => {
   const gameboard = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O'];
-  
-  const cell1 = document.querySelector('#cell1');
-  const cell2 = document.querySelector('#cell2');
-  
+  // make a loop later that splices the array until none are left;
   const printX = () => gameboard[0];
   const printO = () => gameboard[1];
   
@@ -14,11 +13,31 @@ const gameboardObj = (() => {
 })();
 
 
-const Player = (name) => {
+const Player = (name, marker) => {
   
-  const getName = () => name;
+  const displayPlayer1 = () => console.log(playerOne.marker);
+  const displayPlayer2 = () => console.log(playerTwo.marker);
+  
+  const playerOne = {
+    name: "Josh",
+    marker: "X"
+  }
+  
+  const playerTwo = {
+    name: "Tim", 
+    marker: "O"
+  }
+  
+  return { displayPlayer1, displayPlayer2 };
   
 };
+/*
+for reference later on. This is how a Factory Function is accessed.
+const taco = Player();
+
+taco.displayPlayer();
+*/
+
 
 const gameFlow = (() => {
   
@@ -26,14 +45,35 @@ const gameFlow = (() => {
 })();
 
 
-function displayUpdate() {
+function updateDisplay() {
+  "use strict"
   
-  cell1.textContent = gameboardObj.printX();
-  cell2.textContent = gameboardObj.printO();
   
   
 }
 
-displayUpdate();
+function clickButton() {
+  cells.forEach(cell => {
+    cell.addEventListener('click', function() {
+      
+      const player = Player();
+      
+      
+      cell.textContent = gameboardObj.printX();
+      
+      
+    });
+  });
+}
+
+clickButton();
+
+
+  
+  
+
+	
+	
+	
   
   
